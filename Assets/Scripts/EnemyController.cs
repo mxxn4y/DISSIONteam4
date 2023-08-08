d90e00ce17bf3730a1ee93971ced750d7711c0d7
Jeong Sujin <wjdtnwls0106@gmail.com>
2023-08-08 21:32:19 +0900

    enemyRoaming
    적 배회 기능 추가

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -5,7 +5,7 @@ using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour
 {
-    public enum CurrentState { idle, trace, attack, dead };
+    public enum CurrentState { idle, roaming, trace, attack, dead };
     public CurrentState curState = CurrentState.idle; //현재상태
 
     private Transform _transform;
@@ -33,10 +33,8 @@ public class EnemyController : MonoBehaviour
         nvAgent = this.gameObject.GetComponent<NavMeshAgent>();
         //위치 설정 -> 추격
 
-        firstPosition = this.transform.position;
-
-        StartCoroutine(this.CheckState());
-        StartCoroutine(this.CheckStateForAction());
+        StartCoroutine(CheckState());
+        StartCoroutine(CheckStateForAction());
 
         player = GameObject.Find("Player").GetComponent<PlayerController>(); //수진
     }
@@ -55,6 +53,7 @@ public class EnemyController : MonoBehaviour
             else if (dist <= traceDist)
             {
                 curState = CurrentState.trace;
+                StopCoroutine(Roaming());
             }
             else
             {
@@ -69,14 +68,19 @@ public class EnemyController : MonoBehaviour
         {
             switch (curState)
             {
-                case CurrentState.idle: //멈춤
-                    nvAgent.destination = firstPosition;
-                    //nvAgent.Stop();
+                case CurrentState.idle: //멈춤(기본상태 - 배회) - 수진
+                    yield return new WaitForSeconds(10f);
+                    curState = CurrentState.roaming;
                     break;
+
+                case CurrentState.roaming:
+                    StartCoroutine(Roaming());
+                    break;
+
                 case CurrentState.trace: //추적
                     nvAgent.destination = playerTransform.position;
-                    //nvAgent.Resume();
                     break;
+
                 case CurrentState.attack: //공격
                     if (canAttack)
                     {
@@ -104,5 +108,50 @@ public class EnemyController : MonoBehaviour
         canAttack = false;
         yield return new WaitForSeconds(attackSpeed);
         canAttack = true;
+    }
+
+    //몬스터 배회 코드 - 수진
+    IEnumerator Roaming()
+    {
+        while (true)
+        {
+            Vector3 randomPosition = GetRandomRoamingPosition();
+            float dist = Vector3.Distance(playerTransform.position, _transform.position);
+
+            if (dist <= traceDist)
+            {
+                curState = CurrentState.trace;
+                StopCoroutine(Roaming());
+                yield break;
+            }
+
+            // 걷는 애니메이션 true
+            while (Vector3.Distance(transform.position, randomPosition) > 0.1f)
+            {
+                transform.LookAt(randomPosition);
+                nvAgent.destination = randomPosition;
+
+                yield return null;
+            }
+
+            // 걷는 애니메이션 flase, idle
+            // 걷는 애니메이션 true
+            yield return new WaitForSeconds(3f);
+
+        }
+    }
+
+    Vector3 GetRandomRoamingPosition()
+    {
+        firstPosition = this.transform.position;
+        Vector3 randomDirection = new Vector3(Random.Range(-1f, 1f), 0f, Random.Range(-1f, 1f)).normalized;
+        Vector3 newPosition = firstPosition + randomDirection * 3f;
+
+        NavMeshHit navHit;
+        if (NavMesh.SamplePosition(newPosition, out navHit, 1.0f, NavMesh.AllAreas))
+        {
+            return navHit.position;
+        }
+        return newPosition;
     }
 }
